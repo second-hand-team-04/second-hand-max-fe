a006b9dfa8cf4ccdf0b29f98b902d631b41137af
bakhacode <bakhamitai@naver.com>
2023-08-21 17:41:40 +0900

    design: 폰트 및 아이콘 세팅

---
+++ src/styles/designSystem.ts
@@ -0,0 +1,35 @@
+// const font = {
+//   displayBold32: "700 32px Pretendard, sans-serif",
+//   displayBold24: "700 24px Pretendard, sans-serif",
+//   displayBold20: "700 20px Pretendard, sans-serif",
+//   displayBold16: "700 16px Pretendard, sans-serif",
+//   displayBold14: "700 14px Pretendard, sans-serif",
+//   displayBold12: "700 12px Pretendard, sans-serif",
+//   displayMD20: "500 20px Pretendard, sans-serif",
+//   displayMD16: "500 16px Pretendard, sans-serif",
+//   displayMD12: "500 12px Pretendard, sans-serif",
+//   selectedBold20: "700 20px Pretendard, sans-serif",
+//   selectedBold16: "700 16px Pretendard, sans-serif",
+//   selectedBold12: "700 12px Pretendard, sans-serif",
+//   availableMD20: "500 20px Pretendard, sans-serif",
+//   availableMD16: "500 16px Pretendard, sans-serif",
+//   availableMD12: "500 12px Pretendard, sans-serif",
+// };
+
+const font = {
+  displayStrong20: "700 20px Noto Sans KR, sans-serif",
+  displayStrong16: "700 16px Noto Sans KR, sans-serif",
+  displayDefault16: "400 16px Noto Sans KR, sans-serif",
+  displayDefault12: "400 12px Noto Sans KR, sans-serif",
+
+  availableStrong16: "700 16px Noto Sans KR, sans-serif",
+  availableStrong12: "700 12px Noto Sans KR, sans-serif",
+  availableStrong10: "700 10px Noto Sans KR, sans-serif",
+  availableDefault16: "400 16px Noto Sans KR, sans-serif",
+  availableDefault12: "400 12px Noto Sans KR, sans-serif",
+
+  enabledStrong16: "700 16px Noto Sans KR, sans-serif",
+  enabledStrong12: "700 12px Noto Sans KR, sans-serif",
+};
+
+
