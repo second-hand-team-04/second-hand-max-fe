02718957200330d0a13152a1aa9323f44652301f
Daeram Chung <daeram.chung@gmail.com>
2023-08-21 18:01:15 +0900

    design: 색상 및 로고 이미지 추가

--- src/styles/designSystem.ts
+++ src/styles/designSystem.ts
@@ -1,20 +1,22 @@
-// const font = {
-//   displayBold32: "700 32px Pretendard, sans-serif",
-//   displayBold24: "700 24px Pretendard, sans-serif",
-//   displayBold20: "700 20px Pretendard, sans-serif",
-//   displayBold16: "700 16px Pretendard, sans-serif",
-//   displayBold14: "700 14px Pretendard, sans-serif",
-//   displayBold12: "700 12px Pretendard, sans-serif",
-//   displayMD20: "500 20px Pretendard, sans-serif",
-//   displayMD16: "500 16px Pretendard, sans-serif",
-//   displayMD12: "500 12px Pretendard, sans-serif",
-//   selectedBold20: "700 20px Pretendard, sans-serif",
-//   selectedBold16: "700 16px Pretendard, sans-serif",
-//   selectedBold12: "700 12px Pretendard, sans-serif",
-//   availableMD20: "500 20px Pretendard, sans-serif",
-//   availableMD16: "500 16px Pretendard, sans-serif",
-//   availableMD12: "500 12px Pretendard, sans-serif",
-// };
+import { decToHexOpacity } from "@utils/style";
+
+const colors = {
+  white: "#FFFFFF",
+  grey50: "FAFAFA",
+  grey100: `#F9F9F9${decToHexOpacity(0.8)}`,
+  grey200: `#F5F5F5${decToHexOpacity(0.7)}`,
+  grey300: `#B3B3B3${decToHexOpacity(0.12)}`,
+  grey400: `#EFEFF0`,
+  grey500: `#B3B3B3${decToHexOpacity(0.39)}`,
+  grey600: `#000000${decToHexOpacity(0.2)}`,
+  grey700: `#3C3C43${decToHexOpacity(0.36)}`,
+  grey800: `#3C3C43${decToHexOpacity(0.6)}`,
+  grey900: "#3C3C43",
+  black: "#000000",
+  mint: "#00C7BE",
+  orange: "#FF9500",
+  red: "#FF3B30",
+};
 
 const font = {
   displayStrong20: "700 20px Noto Sans KR, sans-serif",
@@ -32,4 +34,40 @@ const font = {
   enabledStrong12: "700 12px Noto Sans KR, sans-serif",
 };
 
-
+export default {
+  color: {
+    neutral: {
+      text: colors.grey900,
+      textWeak: colors.grey800,
+      textStrong: colors.black,
+      background: colors.white,
+      backgroundWeak: colors.grey50,
+      backgroundBold: colors.grey400,
+      backgroundBlur: colors.grey100, // needs backdrop-filter: blur(8px);
+      border: colors.grey500,
+      borderStrong: colors.grey700,
+      overlay: colors.grey600,
+    },
+    accent: {
+      text: colors.white,
+      textWeak: colors.black,
+      backgroundPrimary: colors.orange,
+      backgroundSecondary: colors.mint,
+    },
+    system: {
+      warning: colors.red,
+      background: colors.white,
+      backgroundWeak: colors.grey200,
+    },
+  },
+  filter: {
+    neutralTextWeak:
+      "brightness(0) saturate(100%) invert(15%) sepia(2%) saturate(5010%) hue-rotate(202deg) brightness(93%) contrast(73%)",
+    accentText:
+      "invert(100%) sepia(97%) saturate(15%) hue-rotate(110deg) brightness(103%) contrast(102%)",
+  },
+  backdropFilter: {
+    blur: "blur(8px)",
+  },
+  font,
+};
---
+++ src/utils/style.ts
@@ -0,0 +1,9 @@
+export const decToHexOpacity = (opacity: number) => {
+  if (opacity < 0 || opacity > 1)
+    throw Error("Opacity needs to be between 0 and 1 (inclusive).");
+
+  return Math.floor(opacity * 255)
+    .toString(16)
+    .toUpperCase()
+    .padStart(2, "0");
+};
