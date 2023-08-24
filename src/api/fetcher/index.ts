a34178b0706aa24be4240db6c3aaa25a91283d77
Daeram Chung <daeram.chung@gmail.com>
2023-08-25 00:44:11 +0900

    #14 feat: Init axios

---
+++ src/api/fetcher/index.ts
@@ -0,0 +1,30 @@
+import axios from "axios";
+
+const BASE_API_URL =
+  process.env.NODE_ENV === "production"
+    ? import.meta.env.VITE_APP_API_URL_PROD
+    : import.meta.env.VITE_API_URL_DEV;
+
+export const fetcher = axios.create({
+  baseURL: `${BASE_API_URL}/api`,
+  headers: { "Content-Type": "application/json" },
+});
+
+// TODO: auth 전략 결정 후 수정 필요
+export const fetcherProtected = axios.create({
+  baseURL: `${BASE_API_URL}/api`,
+  headers: { "Content-Type": "application/json" },
+});
+
+fetcherProtected.interceptors.request.use(
+  (config) => {
+    const accessToken = localStorage.getItem("accessToken");
+    if (accessToken) {
+      config.headers["Authorization"] = `Bearer ${accessToken}`;
+    }
+    return config;
+  },
+  (error) => {
+    Promise.reject(error);
+  }
+);
