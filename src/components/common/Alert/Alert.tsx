574416a417525a98cb1d1cb76c885f9a8adb3ef4
bakhacode <bakhamitai@naver.com>
2023-08-24 18:31:58 +0900

    #8 feat: Alert 컴포넌트 구현

---
+++ src/components/common/Alert/Alert.tsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { styled } from "styled-components";
+
+type Props = {
+  text: string;
+  buttons: React.ReactNode[];
+};
+
+export default function Alert({ text, buttons }: Props) {
+  return (
+    <StyledAlert>
+      <TextArea>{text}</TextArea>
+      <ButtonContainer>
+        {buttons.map((button, index) => (
+          <React.Fragment key={index}>{button}</React.Fragment>
+        ))}
+      </ButtonContainer>
+    </StyledAlert>
+  );
+}
+
+const StyledAlert = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 336px;
+  border-radius: 16px;
+  box-shadow: 0px 4px 4px 0px rgba(0, 0, 0, 0.25);
+`;
+
+const alertBody = `
+  display: flex;
+  padding: 24px 32px;
+  align-items: center;
+  gap: 32px;
+`;
+
+const TextArea = styled.div`
+  ${alertBody}
+  justify-content: flex-start;
+  font: ${({ theme: { font } }) => font.displayStrong16};
+  color: ${({ theme: { color } }) => color.neutral.textStrong};
+`;
+
+const ButtonContainer = styled.div`
+  ${alertBody}
+  justify-content: flex-end;
+`;
