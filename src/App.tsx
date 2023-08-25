d0f217ab7799e92b6698fe9910f49f281fb91ffc
bakhacode <bakhamitai@naver.com>
2023-08-25 11:13:56 +0900

    #4 refactor: DOM 렌더링 방지를 위한 transient props 사용

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from "styled-components";
 import "./App.css";
 import GlobalStyles from "@styles/GlobalStyles";
 import designSystem from "@styles/designSystem";
+import { Tag } from "@components/common/Tag/Tag";
 
 function App() {
   return (
     <ThemeProvider theme={designSystem}>
       <GlobalStyles />
+      <Tag title="Tag" isSelected={true} />
     </ThemeProvider>
   );
 }
--- src/components/common/Tag/Tag.tsx
+++ src/components/common/Tag/Tag.tsx
@@ -7,10 +7,10 @@ export function Tag({
   title: string;
   isSelected: boolean;
 }) {
-  return <TagButton isSelected={isSelected}>{title}</TagButton>;
+  return <TagButton $isSelected={isSelected}>{title}</TagButton>;
 }
 
-const TagButton = styled.button<{ isSelected: boolean }>`
+const TagButton = styled.button<{ $isSelected: boolean }>`
   display: flex;
   height: 32px;
   padding: 0px 16px;
@@ -19,10 +19,10 @@ const TagButton = styled.button<{ isSelected: boolean }>`
   border-radius: 50px;
   font: ${({ theme: { font } }) => font.displayDefault12};
   border: 1px solid
-    ${({ isSelected, theme: { color } }) =>
-      isSelected ? "none" : color.neutral.border};
-  color: ${({ isSelected, theme: { color } }) =>
-    isSelected ? color.accent.text : color.accent.textWeak};
-  background: ${({ isSelected, theme: { color } }) =>
-    isSelected ? color.accent.backgroundPrimary : color.accent.text};
+    ${({ $isSelected, theme: { color } }) =>
+      $isSelected ? "none" : color.neutral.border};
+  color: ${({ $isSelected, theme: { color } }) =>
+    $isSelected ? color.accent.text : color.accent.textWeak};
+  background: ${({ $isSelected, theme: { color } }) =>
+    $isSelected ? color.accent.backgroundPrimary : color.accent.text};
 `;
