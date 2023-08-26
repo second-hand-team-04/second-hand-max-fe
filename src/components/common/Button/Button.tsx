affd3290869347b667668b0e75c872ed07f14e5f
Daeram Chung <daeram.chung@gmail.com>
2023-08-26 17:26:23 +0900

    #17 feat: Button 컴포넌트 구현

---
+++ src/components/common/Button/Button.tsx
@@ -0,0 +1,64 @@
+import { ReactNode, ButtonHTMLAttributes } from "react";
+import { ContainedButton, OutlinedButton, PlainButton } from "./ButtonVariants";
+import { Variant, Size } from "./types";
+import { BackgroundColor, BorderColor, BorderRadius, Color } from "../types";
+
+type Props = {
+  variant?: Variant;
+  size?: Size;
+  backgroundColor?: BackgroundColor | "";
+  borderColor?: BorderColor | "";
+  borderRadius?: BorderRadius;
+  color?: Color | "";
+  children: ReactNode;
+} & ButtonHTMLAttributes<HTMLButtonElement>;
+
+export default function Button({
+  variant = "contained",
+  size = "M",
+  backgroundColor = "",
+  borderColor = "",
+  borderRadius = "",
+  color = "",
+  children,
+  ...props
+}: Props) {
+  if (variant === "plain") {
+    return (
+      <PlainButton
+        $size={size}
+        $backgroundColor={backgroundColor}
+        $color={color}
+        {...props}>
+        {children}
+      </PlainButton>
+    );
+  }
+
+  if (variant === "contained") {
+    return (
+      <ContainedButton
+        $size={size}
+        $backgroundColor={backgroundColor}
+        $borderRadius={borderRadius}
+        $color={color}
+        {...props}>
+        {children}
+      </ContainedButton>
+    );
+  }
+
+  if (variant === "outlined") {
+    return (
+      <OutlinedButton
+        $size={size}
+        $backgroundColor={backgroundColor}
+        $borderColor={borderColor}
+        $borderRadius={borderRadius}
+        $color={color}
+        {...props}>
+        {children}
+      </OutlinedButton>
+    );
+  }
+}
