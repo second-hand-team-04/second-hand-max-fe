773aabc30cbe242ae1459b3773adfc4bfd94c2e1
Daeram Chung <daeram.chung@gmail.com>
2023-08-28 14:58:02 +0900

    #16 chore: React query client 설정

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,17 @@ import "./App.css";
 import GlobalStyles from "@styles/GlobalStyles";
 import designSystem from "@styles/designSystem";
 import { Tag } from "@components/common/Tag/Tag";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <ThemeProvider theme={designSystem}>
       <GlobalStyles />
-      <Tag title="Tag" isSelected={true} />
+      <QueryClientProvider client={queryClient}>
+        <Tag title="Tag" isSelected={true} />
+      </QueryClientProvider>
     </ThemeProvider>
   );
 }
