c10e2a9055c847cbeb9471bc4fce53dbef1838d9
Daeram Chung <daeram.chung@gmail.com>
2023-08-28 15:05:39 +0900

    React Query 설정 (#25)
    * #16 chore: tanstack query eslint plugin 적용
    
    * #16 chore: React query client 설정

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,11 +5,12 @@ module.exports = {
     "eslint:recommended",
     "plugin:@typescript-eslint/recommended",
     "plugin:react-hooks/recommended",
+    "plugin:@tanstack/eslint-plugin-query/recommended",
     "prettier",
   ],
   ignorePatterns: ["dist", ".eslintrc.cjs"],
   parser: "@typescript-eslint/parser",
-  plugins: ["react-refresh", "eslint-plugin-react"],
+  plugins: ["react-refresh", "eslint-plugin-react", "@tanstack/query"],
   rules: {
     "react-refresh/only-export-components": [
       "warn",
@@ -22,5 +23,7 @@ module.exports = {
         html: true,
       },
     ],
+    "@tanstack/query/exhaustive-deps": "error",
+    "@tanstack/query/prefer-query-object-syntax": "error",
   },
 };
