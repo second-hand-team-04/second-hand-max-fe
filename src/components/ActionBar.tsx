4495e0fa85031bb0122fc456623817d2b2bf2bc0
bakhacode <bakhamitai@naver.com>
2023-08-29 15:17:08 +0900

    #35 design: css 컨벤션 적용 및 포지션 fixed

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -5,12 +5,13 @@ export default function ActionBar({ children }: { children: React.ReactNode }) {
 }
 
 const StyledActionBar = styled.div`
+  width: inherit;
+  height: 64px;
+  padding: 16px;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  width: 393px;
-  height: 64px;
-  padding: 16px;
+  position: fixed;
 
   background: ${({ theme: { color } }) => color.neutral.backgroundWeak};
   border-top: ${({ theme: { color } }) => `1px solid ${color.neutral.border}`};
