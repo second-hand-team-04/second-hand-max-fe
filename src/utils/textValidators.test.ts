4aa29946e28409fcf1644e5aeb2547fea41ffd9a
Daeram Chung <daeram.chung@gmail.com>
2023-08-30 01:19:45 +0900

    #39 test: 닉네임, 이메일, 비밀번호 validator 테스트 코드 작성

---
+++ src/utils/textValidators.test.ts
@@ -0,0 +1,38 @@
+import {
+  validateEmail,
+  validateNickname,
+  validatePassword,
+} from "./textValidators";
+
+describe("validates nickname", () => {
+  it("should not throw an error for a valid nickname", () => {
+    expect(validateNickname("kakamotobi")).toBe(undefined);
+  });
+  it("should not throw an error for a valid nickname", () => {
+    expect(validateNickname("카카모토비")).toBe(undefined);
+  });
+  it("should throw an error for an invalid nickname (too short)", () => {
+    expect(() => validateNickname("k")).toThrowError();
+  });
+});
+
+describe("validates email", () => {
+  it("should not throw an error for a valid email", () => {
+    expect(validateEmail("test@test.com")).toBe(undefined);
+  });
+  it("should throw an error for an invalid email", () => {
+    expect(() => validateEmail("test@test")).toThrowError();
+  });
+});
+
+describe("validates password", () => {
+  it("should not throw an error for a valid email", () => {
+    expect(validatePassword("iampassword$")).toBe(undefined);
+  });
+  it("should throw an error for an invalid email (too short)", () => {
+    expect(() => validatePassword("pass$")).toThrowError();
+  });
+  it("should throw an error for an invalid email (no special character)", () => {
+    expect(() => validatePassword("iampassword")).toThrowError();
+  });
+});
