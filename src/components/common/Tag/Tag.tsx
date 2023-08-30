fbbc0c2aafde4dcb821e8a4e1c6750c0aaa375ff
bakhacode <bakhamitai@naver.com>
2023-08-31 01:17:32 +0900

    refactor: 태그 props, 구조 수정

--- src/components/common/Tag/Tag.tsx
+++ src/components/common/Tag/Tag.tsx
@@ -1,16 +1,24 @@
 import { styled } from "styled-components";
 
 export function Tag({
-  title,
+  children,
+  tag,
   isSelected,
+  onClick,
 }: {
-  title: string;
+  children: React.ReactNode;
+  tag: { id: number; title: string; imageUrl: string };
   isSelected: boolean;
+  onClick?: (tagTitle: string) => void;
 }) {
-  return <TagButton $isSelected={isSelected}>{title}</TagButton>;
+  return (
+    <StyledTag onClick={() => onClick?.(tag.title)} $isSelected={isSelected}>
+      {children}
+    </StyledTag>
+  );
 }
 
-const TagButton = styled.button<{ $isSelected: boolean }>`
+const StyledTag = styled.button<{ $isSelected: boolean }>`
   display: flex;
   height: 32px;
   padding: 0px 16px;
