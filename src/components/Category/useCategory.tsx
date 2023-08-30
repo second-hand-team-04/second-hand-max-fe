70610dc9b824c6f5d68430d81dc77d0436c9ae23
bakhacode <bakhamitai@naver.com>
2023-08-31 01:18:13 +0900

    feat: useCategory 커스텀 훅 생성

---
+++ src/components/Category/useCategory.tsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from "react";
+
+export type CategoryType = {
+  id: number;
+  title: string;
+  imageUrl: string;
+};
+
+export default function useCategory(categoryList: CategoryType[]) {
+  const initialCategories = getRandomSubarray(categoryList, 3);
+  const initialSelection = initialCategories[0].title;
+
+  const [categories, setCategories] = useState(initialCategories);
+  const [selectedCategory, setSelectedCategory] = useState(initialSelection);
+
+  useEffect(() => {
+    if (!selectedCategory) return;
+
+    const filteredList = categoryList.filter(
+      (category) => category.title !== selectedCategory
+    );
+    const selectedCategoryObject = categoryList.find(
+      (category) => category.title === selectedCategory
+    );
+
+    if (!selectedCategoryObject) return;
+
+    const twoRandomCategories = getRandomSubarray(filteredList, 2);
+    const finalThreeCategories = [
+      selectedCategoryObject,
+      ...twoRandomCategories,
+    ];
+
+    setCategories(finalThreeCategories);
+  }, [selectedCategory, categoryList]);
+
+  return { selectedCategory, categories, setSelectedCategory };
+}
+
+function getRandomSubarray(
+  arr: { id: number; title: string; imageUrl: string }[],
+  n: number
+) {
+  const shuffled = arr.slice();
+
+  for (let i = shuffled.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+  }
+
+  return shuffled.slice(0, n);
+}
