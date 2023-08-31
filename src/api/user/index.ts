2d6d25d6b90fa45362f8e74a05f1dbc75558723f
Daeram Chung <daeram.chung@gmail.com>
2023-08-31 14:34:33 +0900

    #39 feat: Signup API 및 mock 추가

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,7 +1,12 @@
-import { fetcherProtected } from "../fetcher";
+import { fetcher, fetcherProtected } from "../fetcher";
 import { User } from "./types";
 
+// TODO: remove this
 export const getUser = async () => {
   const res = await fetcherProtected.get<User>("/user");
   return res.data;
+};
+
+export const postSignUp = async (body: FormData) => {
+  return await fetcher.post<null>("/users/signup", body);
 };
