96e83d589749c87e2613c169c4c6907231077494
bakhacode <bakhamitai@naver.com>
2023-08-31 16:42:15 +0900

    feat: 카테고리 페이지 쿼리 적용

--- src/api/queries/queryKeys.ts
+++ src/api/queries/queryKeys.ts
@@ -1,3 +1,7 @@
 export const userKeys = {
   user: ["user"] as const,
 };
+
+export const categoryKeys = {
+  categories: ["categories"] as const,
+};
--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,5 +1,5 @@
 import { fetcher, fetcherProtected } from "../fetcher";
-import { User } from "./types";
+import { CategoryType, User } from "./types";
 
 // TODO: remove this
 export const getUser = async () => {
@@ -9,4 +9,9 @@ export const getUser = async () => {
 
 export const postSignUp = async (body: FormData) => {
   return await fetcher.post<null>("/users/signup", body);
+};
+
+export const getCategories = async () => {
+  const res = await fetcher.get<CategoryType[]>("/categories");
+  return res.data;
 };
