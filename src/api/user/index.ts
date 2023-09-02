c19ed25b54c6f979e9aee9f2edd0fefb15fdd82f
Daeram Chung <daeram.chung@gmail.com>
2023-09-02 16:53:50 +0900

    #50 refactor: fetcherProtected 제거

--- src/api/user/index.ts
+++ src/api/user/index.ts
@@ -1,11 +1,21 @@
 import { Response } from "api/types";
 import { fetcher } from "../fetcher";
 
-export type User = {
-  nickname: string;
-  profileUrl: string;
+type SignInData = {
+  accessToken: string;
+  refreshToken: string;
+  user: {
+    nickname: string;
+    profilePicture: string;
+  };
 };
 
 export const postSignUp = async (body: FormData) => {
-  return await fetcher.post<Response<null>>("/users/signup", body);
+  const res = await fetcher.post<Response<null>>("/users/signup", body);
+  return res.data;
+};
+
+export const postSignIn = async (body: { email: string; password: string }) => {
+  const res = await fetcher.post<Response<SignInData>>("/api/auth", body);
+  return res.data;
 };
--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,17 +1,7 @@
 import { rest } from "msw";
-import { User } from "api/user/index";
 import { categoriesData, regionListData } from "./data";
 
 export default [
-  // TODO: remove this
-  rest.get("/api/user", async (_, res, ctx) => {
-    return res(ctx.status(400));
-    return res(
-      ctx.status(200),
-      ctx.json<User>({ nickname: "kakamotobi", profileUrl: "" })
-    );
-  }),
-
   rest.post("/api/users/signin", async (_, res, ctx) => {
     return res(ctx.status(201), ctx.json(null));
   }),
