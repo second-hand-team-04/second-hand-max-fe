2b2df68ad6b1ccb2732dbeed39e1cd700574aa43
Daeram Chung <daeram.chung@gmail.com>
2023-10-06 12:30:18 +0900

    style: 주석 제거

--- src/api/queries/useWishlistItemAddMutation.ts
+++ src/api/queries/useWishlistItemAddMutation.ts
@@ -4,13 +4,10 @@ import toast from "react-hot-toast";
 import queryKeys from "./queryKeys";
 
 export default function useWishlistItemAddMutation(id: number) {
-  // const queryClient = useQueryClient();
-
   return useMutation({
     mutationKey: queryKeys.wishlist.add().queryKey,
     mutationFn: () => postWishlistItem(id),
     onSuccess: () => {
-      // queryClient.invalidateQueries(queryKeys.productItems.detail(id).queryKey);
       toast.success("관심 목록에 추가되었습니다");
     },
   });
--- src/api/queries/useWishlistItemRemoveMutation.ts
+++ src/api/queries/useWishlistItemRemoveMutation.ts
@@ -4,14 +4,10 @@ import toast from "react-hot-toast";
 import queryKeys from "./queryKeys";
 
 export default function useWishlistItemRemoveMutation(id: number) {
-  // const queryClient = useQueryClient();
-
   return useMutation({
     mutationKey: queryKeys.wishlist.remove().queryKey,
     mutationFn: () => deleteWishlistItem(id),
     onSuccess: () => {
-      // queryClient.invalidateQueries(queryKeys)
-      // queryClient.invalidateQueries(queryKeys.productItems.detail(id).queryKey);
       toast.success("관심 목록에서 삭제되었습니다");
     },
   });
